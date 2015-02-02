0eabfd2226a9bc8ddf33a9e3b24a7db68de69a64
Michael McDevitt <mmcdevi1@gmail.com>
2015-02-02 14:52:53 -0500

    edit user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   validates_presence_of :name
   validates   :phone, presence: true, 
-                      length: { minimum: 10 },
+                      length: { minimum: 10, maximum: 10 },
                       format: {
                         with: /\d\d\d\d\d\d\d\d\d\d/, message: " number must not contain any special characters. Ex - 9141231234"
                       }
